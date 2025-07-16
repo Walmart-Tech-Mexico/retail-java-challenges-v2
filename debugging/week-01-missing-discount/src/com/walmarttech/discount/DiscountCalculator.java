f1c3623ae4fcb751973fe44bc8b13c32e374483a
Carlos Estrada <carabesle@gmail.com>
2025-07-15 18:32:02 -0600

    refactor: change membershipLevel from string to new Enum

--- debugging/week-01-missing-discount/src/com/walmarttech/discount/DiscountCalculator.java
+++ debugging/week-01-missing-discount/src/com/walmarttech/discount/DiscountCalculator.java
@@ -1,7 +1,9 @@
+package com.walmarttech.discount;
+
 public class DiscountCalculator {
 
     public double calculateDiscount(Customer customer, double totalAmount) {
-        if (customer.getMembershipLevel() == "Gold") {
+        if (customer.getMembershipLevel() == MembershipLevel.GOLD) {
             return totalAmount * 0.2; // 20% de descuento
         }
         return 0.0;
