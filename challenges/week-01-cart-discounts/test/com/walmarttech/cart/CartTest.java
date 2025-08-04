df42842971ee070218a8de9e319a4ae57c503714
Carlos Alonso <alonsogonzalezgomez97@gmail.com>
2025-08-04 12:39:31 -0600

    week-01-cart-discounts - Solved the discount challenge

--- challenges/week-01-cart-discounts/test/com/walmarttech/cart/CartTest.java
+++ challenges/week-01-cart-discounts/test/com/walmarttech/cart/CartTest.java
@@ -1,6 +1,9 @@
+
 import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CartTest {
 
